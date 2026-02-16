089258d25c809c26e69b67f8edb69987cf71e4b7
ArmandoAlvarezP <armando.alvarez.gkm@hotmail.com>
2026-02-15 21:32:15 -0600

    Indentaciones corregidas en la versión de inicio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,29 +5,29 @@ import Image from 'react-bootstrap/Image';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 const Header = () => {
-  return (
-    <header>
-        <Navbar className="header" expand="sm" fixed='top' variant='dark' data-bs-theme="dark">
-        <Container>
-            <Navbar.Brand href="#home">
-                <Row>
-                    <Col lg={4} xs={8} md={6} sm={6} >
-                        <Image src="/logo.jpeg" width='180px' height='100px' rounded/>
-                    </Col>
-                </Row>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link href="#nosotros">Nosotros</Nav.Link>
-                <Nav.Link href="#merch">Merch</Nav.Link>
-                <Nav.Link href="#musica">Música</Nav.Link>
-            </Nav>
-            </Navbar.Collapse>
-        </Container>
-        </Navbar>
-    </header>
-  )
+    return (
+        <header>
+            <Navbar className="header" expand="sm" fixed='top' variant='dark' data-bs-theme="dark">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <Row>
+                        <Col lg={4} xs={8} md={6} sm={6} >
+                            <Image src="/logo.jpeg" width='180px' height='100px' rounded/>
+                        </Col>
+                    </Row>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#nosotros">Nosotros</Nav.Link>
+                    <Nav.Link href="#merch">Merch</Nav.Link>
+                    <Nav.Link href="#musica">Música</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
+        </header>
+    )
 }
 
 export default Header
--- src/components/Musica.jsx
+++ src/components/Musica.jsx
@@ -3,25 +3,25 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 const Musica = () => {
-  return (
-    <>
-        <section id="musica" className="musica">
-            <Container>
-                <Row>
-                    <Col>
-                        <iframe data-testid="embed-iframe" style={{borderRadius:'12px'}} 
-                            src="https://open.spotify.com/embed/artist/79kB2wRealQ7TgVXSyFp6D?utm_source=generator&theme=0" 
-                            width="100%" height="352" frameBorder="0" allowfullscreen="" 
-                            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-                            loading="lazy"
-                        >
-                        </iframe>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    </>
-  )
+    return (
+        <>
+            <section id="musica" className="musica">
+                <Container>
+                    <Row>
+                        <Col>
+                            <iframe data-testid="embed-iframe" style={{borderRadius:'12px'}} 
+                                src="https://open.spotify.com/embed/artist/79kB2wRealQ7TgVXSyFp6D?utm_source=generator&theme=0" 
+                                width="100%" height="352" frameBorder="0" allowfullscreen="" 
+                                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                                loading="lazy"
+                            >
+                            </iframe>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        </>
+    )
 }
 
 export default Musica
